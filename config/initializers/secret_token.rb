0d6bc902845280c1e61200b3406e78e5febf761c
Ben Nicholas <ben@ben-nicholas.com>
2011-09-12 18:55:17 -0400

    Created a rails 3.1 app for wrapping up the hacks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NextBusBackend::Application.config.secret_token = '038b3f9de60818ed8d762b3f4e10b471bb199bad072af61d40d1ac61bd6d403055586aefa7423025025e9f5792428c9edef58c315cb0399b2e0ed079332ab007'
