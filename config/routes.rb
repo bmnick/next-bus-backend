a51dc237f382265bf5ad7858a3b6735a95dd62d6
Ben Nicholas <ben@ben-nicholas.com>
2011-09-14 11:49:11 -0400

    Welcome scaffolding for eventual trimming

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NextBusBackend::Application.routes.draw do
+  resources :welcomes
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ NextBusBackend::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
